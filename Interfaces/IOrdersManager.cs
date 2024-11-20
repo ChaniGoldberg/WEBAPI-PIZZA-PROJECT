96098d5210d01ad0ed13a25690af500e0790381a
ChaniGoldberg <clg33102@gmail.com>
2024-11-20 13:37:59 +0200

    second verision

--- Interfaces/IOrdersManager.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-using תשתית_לניהול_חנות_פיצה_חני_גולדברג;
-
-public interface IOrdersManager{
-void setOrder(string CustomerId, DateTime Date,decimal TotalAmount);
-}
---
+++ myFolders/תשתית לניהול חנות פיצה-חני גולדברג/Program.cs
@@ -0,0 +1,33 @@
+
+using myModels.Interfaces; 
+using myServices.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IpizzaMannager,ChanisPizzaServices>();
+builder.Services.AddTransient<IOrdersManager,OrderServices>();
+builder.Services.AddScoped<IWorker,WorkerService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
